61bad33679baccaeff133f646d508904835a9a2c
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-03-18 20:45:48 -0500

    Start basic vue app

---
+++ 03-indecision-app/src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import CounterApp from './components/CounterApp.vue'
+</script>
+
+<template>
+  <h1>My First App</h1>
+  <hr />
+
+  <CounterApp />
+</template>
---
+++ 03-indecision-app/src/components/CounterApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button @click="counter++">+1</button>
+      <button @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+const counter = ref(8)
+const squareCounter = computed(() => counter.value * counter.value)
+</script>
