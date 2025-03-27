6d5e76ddaa71dd444654f37e8948592d0ca1f4ea
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-03-26 22:27:35 -0500

    Add tests for incrementing and decrementing counter

--- 03-indecision-app/tests/components/CounterApp.test.ts
+++ 03-indecision-app/tests/components/CounterApp.test.ts
@@ -26,4 +26,39 @@ describe('<CounterApp />', () => {
       `Square: ${value * value}`,
     )
   })
+
+  test('increment the counter when +1 button is clicked', async () => {
+    const value = 10
+    const wrapper = mount(CounterApp, {
+      props: {
+        value: value,
+      },
+    })
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3')
+
+    const btnIncrement = wrapper.find('button')
+    await btnIncrement.trigger('click')
+
+    expect(counterLabel.text()).toContain(`Counter: ${value + 1}`)
+    expect(squareLabel.text()).toContain(`Square: ${(value + 1) * (value + 1)}`)
+  })
+
+  test('decrement the counter when -1 button is clicked twice', async () => {
+    const value = 10
+    const wrapper = mount(CounterApp, {
+      props: {
+        value: value,
+      },
+    })
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3')
+    const btnDecrement = wrapper.findAll('button')[1]
+
+    await btnDecrement.trigger('click')
+    await btnDecrement.trigger('click')
+
+    expect(counterLabel.text()).toContain(`Counter: ${value - 2}`)
+    expect(squareLabel.text()).toContain(`Square: ${(value - 2) * (value - 2)}`)
+  })
 })
